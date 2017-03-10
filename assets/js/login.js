aa01d9b19e61ddc4e857c3a653c638ff799ae121
RuthMery <ruth.cardenas.perez@gmail.com>
2017-03-10 17:49:40 -0500

    Validación de login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,26 @@
 window.addEventListener('load',function() {
   var loginButton = document.getElementById('login');
   loginButton.addEventListener('click',function() {
+    //Obteniendo los nodos del HTML
     var emailInput = document.getElementById('email');
     var passwordInput = document.getElementById('password');
+    var warnings = document.getElementsByClassName('warning');
+    //Validación: No valores vacíos
+    warnings[0].innerHTML = emailInput.value == ""? "El campo de usuario no puede estar en blanco":"";
+    warnings[1].innerHTML = passwordInput.value == ""? "El campo de contraseña no puede estar en blanco":"";
+    //Validación: Formato correcto de email
+    if (/([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})/.test(emailInput.value) == false && passwordInput.value != "") {
+      warnings[0].innerHTML = "Ingresar un usuario válido";
+    }
+    if (/([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})/.test(emailInput.value) == true && passwordInput.value != "") {
+      if(validateUser(emailInput.value, passwordInput.value)){
+        alert("Hola usuario");
+      }else {
+        alert("Error en el usuario o contraseña");
+      }
+    }
+    //"El campo de usuario no puede estar en blanco"
+    //"Debe ingresar un email válido"
+    //"El campo de contraseña no puede estar en blanco"
   });
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
+//VARIABLES GLOBALES
+  var users = [];
 //FUNCIONES DEL USUARIO
-  function validateUser(paramEmail, paramPassword){
-    alert(paramEmail + " " + paramPassword);
+  function User(name, lastName, nickName, email, password){
+    this.name = name,
+    this.lastName = lastName,
+    this.nickName = nickName,
+    this.email = email,
+    this.password = password;
+    this.login = function (passwordAux) {
+      return this.password == passwordAux? true : false;
+    }
+  }
+  //Creación de usuarios
+  users.push(new User("Mercedes", "Zubieta", "Meche", "meche.zubieta@gmail.com", "meche"));
+  users.push(new User("Arabela", "Rojas", "Ara", "ara.rojas@gmail.com", "ara"));
+  users.push(new User("Maria Paula", "Rivarola", "Papu", "papu.rivarola@gmail.com", "papu"));
+  users.push(new User("Gian Carlo", "Corzo", "Gian", "gian.corzo@gmail.com", "gian"));
+  users.push(new User("Michelle", "Seguil", "mich", "mich.seguil@gmail.com", "mich"));
+  function validateUser(emailUser, passwordUser){
+    for (var user in users) {
+      if (users[user].email == emailUser) {
+        if(users[user].login(passwordUser)){
+          return true;
+        }
+      }
+    }
+    return false;
   }
